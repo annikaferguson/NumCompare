ce1fb7d155a70197107c6b871ddd18731a3e0af8
annikaferguson <fergusak@mail.uc.edu>
2023-06-09 15:28:53 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double input1 = 0;
+        double input2 = 0;
+
+        System.out.println("Enter two numbers: ");
+        input1 = in.nextDouble();
+        input2 = in.nextDouble();
+
+        if(input1 == input2)
+        {
+            System.out.println("The two numbers are equal!");
+        }
+        else if(input1 >= input2)
+        {
+            System.out.println(input1 + " is greater than " + input2);
+        }
+        else if(input1 <= input2)
+        {
+            System.out.println(input1 + " is less than " + input2);
+        }
     }
 }
